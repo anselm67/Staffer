4019a10528e2c5efbc4df096d22a42f846b5fea1
anselm <abairdsmith@gmail.com>
2025-03-10 11:32:23 +0100

    Added prediction_test and full test()

--- light.py
+++ light.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
-from typing import Any, Optional
+from dataclasses import replace
+from typing import cast
 
 import click
 import cv2
 import lightning as L
 import torch
 from torch import Tensor, nn, optim, utils
-from torchvision.io import decode_image
 
 from dataset import StaffDataset
 from model import Config, ViT
@@ -16,9 +15,9 @@ from model import Config, ViT
 
 class LitStaffer(L.LightningModule):
 
-    def __init__(self, model: nn.Module):
+    def __init__(self, config: Config):
         super().__init__()
-        self.model = model
+        self.model = ViT(config)
         self.loss_f = nn.BCEWithLogitsLoss()
 
     def training_step(self, batch: tuple[Tensor, Tensor], batch_idx: int) -> Tensor:
@@ -34,6 +33,10 @@ class LitStaffer(L.LightningModule):
         val_loss = self.loss_f(yhat, gt)
         self.log("val_loss", val_loss)
         return val_loss
+
+    def predict_step(self, image: Tensor) -> Tensor:
+        yhat = self.model(image.unsqueeze(0)).squeeze(0)
+        return (yhat > 0.5).to(torch.float32)
 
     def configure_optimizers(self) -> optim.Adam:
         return optim.Adam(self.parameters(), lr=1e-4)
@@ -67,40 +70,36 @@ def train():
         valid_ds, num_workers=4, batch_size=config.batch_size
     )
 
-    staffer = LitStaffer(ViT(config))
+    staffer = LitStaffer(config)
     trainer = L.Trainer(max_epochs=16)
     trainer.fit(staffer, train_loader, valid_loader)
 
 
 @click.command()
 @click.argument("checkpoint", type=str)
-@click.argument("image_path",
-                type=click.Path(file_okay=True, dir_okay=False, exists=True),
-                required=False)
-def predict(checkpoint: str, image_path: Optional[Path] = None):
-    config = Config()
+def test(checkpoint: str):
+    config = replace(Config(), batch_size=4)
 
-    ds, _ = StaffDataset.create()
+    ds, _ = StaffDataset.create(config=config)
     loader = utils.data.DataLoader(
         ds, num_workers=4, batch_size=config.batch_size)
-    model = ViT(config)
-    LitStaffer.load_from_checkpoint(checkpoint, model=model)
-
-    device = next(model.parameters()).device
+    model = LitStaffer.load_from_checkpoint(checkpoint, config=config)
+    trainer = L.Trainer()
 
     for images, _ in loader:
-
-        # Un-Squeezes for a batch_size of 1.
-        yhats = model(images.to(device))
-        yhats = (yhats > 0.5).to(torch.float32)
-
-        for idx in range(0, config.batch_size):
-            image, yhat = images[idx], yhats[idx]
-
+        yhats = cast(list[Tensor], trainer.predict(model, [images]))
+        for image, yhat in zip(images.unbind(0), yhats):
             cv2.imshow("image", image.squeeze(0).cpu().numpy())
             cv2.imshow("staff", yhat.cpu().numpy())
             if cv2.waitKey(0) == ord('q'):
                 return
+
+
+@click.command()
+def stats():
+    config = replace(Config(), valid_split=0)
+    ds, _ = StaffDataset.create(config=config)
+    ds.stats()
 
 
 @click.group()
@@ -109,8 +108,9 @@ def cli():
 
 
 cli.add_command(train)
-cli.add_command(predict)
+cli.add_command(test)
 cli.add_command(show)
+cli.add_command(stats)
 
 if __name__ == "__main__":
     torch.set_float32_matmul_precision("high")
