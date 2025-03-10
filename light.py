a7118186de46be693f6caf8724b031c51b3569e0
anselm <abairdsmith@gmail.com>
2025-03-10 16:08:11 +0100

    Added acuracy metrics.

--- light.py
+++ light.py
@@ -13,6 +13,15 @@ from dataset import StaffDataset
 from model import Config, ViT
 
 
+def accuracy(pred: Tensor, gt: Tensor) -> tuple[float, float]:
+    true_positives = (pred * gt).sum()
+    false_positives = (pred * (1 - gt)).sum()
+    false_negatives = ((1 - pred) * gt).sum()
+    precision = true_positives / (true_positives + false_positives + 1e-8)
+    recall = true_positives / (true_positives + false_negatives + 1e-8)
+    return precision.item(), recall.item()
+
+
 class LitStaffer(L.LightningModule):
 
     def __init__(self, config: Config):
@@ -31,7 +40,10 @@ class LitStaffer(L.LightningModule):
         y, gt = batch
         yhat = self.model(y)
         val_loss = self.loss_f(yhat, gt)
+        val_precision, val_recall = accuracy((yhat > 0.5).to(torch.float), gt)
         self.log("val_loss", val_loss)
+        self.log("val_precision", val_precision)
+        self.log("val_recall", val_recall)
         return val_loss
 
     def predict_step(self, image: Tensor) -> Tensor:
@@ -59,19 +71,20 @@ def show():
 
 
 @click.command()
-def train():
+@click.option("epochs", "-e", type=int, default=32)
+def train(epochs: int):
     config = Config()
 
     train_ds, valid_ds = StaffDataset.create()
     train_loader = utils.data.DataLoader(
-        train_ds, num_workers=4, batch_size=config.batch_size
+        train_ds, num_workers=4, batch_size=config.batch_size, shuffle=True
     )
     valid_loader = utils.data.DataLoader(
         valid_ds, num_workers=4, batch_size=config.batch_size
     )
 
     staffer = LitStaffer(config)
-    trainer = L.Trainer(max_epochs=16)
+    trainer = L.Trainer(max_epochs=epochs, limit_val_batches=10)
     trainer.fit(staffer, train_loader, valid_loader)
 
 
@@ -86,9 +99,11 @@ def test(checkpoint: str):
     model = LitStaffer.load_from_checkpoint(checkpoint, config=config)
     trainer = L.Trainer()
 
-    for images, _ in loader:
+    for images, gts in loader:
         yhats = cast(list[Tensor], trainer.predict(model, [images]))
-        for image, yhat in zip(images.unbind(0), yhats):
+        for image, yhat, gt in zip(images.unbind(0), yhats, gts):
+            precision, recall = accuracy(yhat, gt)
+            print(f"P: {100 * precision:.2f}%, R: {100 * recall:.2f}%")
             cv2.imshow("image", image.squeeze(0).cpu().numpy())
             cv2.imshow("staff", yhat.cpu().numpy())
             if cv2.waitKey(0) == ord('q'):
